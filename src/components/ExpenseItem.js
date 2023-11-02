49c531a750bf6b49e158e90a4b5a4a1d005952b1
Rishabh Sharma <142036008+rs9209@users.noreply.github.com>
2023-11-02 11:40:30 +0530

    Update ExpenseItem.js

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -42,8 +42,8 @@ const ExpenseItem = (props) => {
         <tr>
         <td>{props.name}</td>
         <td>{currency}{props.cost}</td>
-        <td><button style={{borderRadius : '50%',color : '#fff',backgroundColor : '#1abc9c',fontWeight : 'bold',fontSize : '1.5rem',padding : '0px'}} onClick={event=> increaseAllocation(props.name)}>+</button></td>
-        <td><button style={{borderRadius : '50%',color : '#fff',backgroundColor : 'red',fontWeight : 'bold'}} onClick={event=> decreaseAllocation(props.name)}><span style={{fontWeight : 'bold',color : '#fff',fontSize : '1.5rem'}} >-</span></button></td>
+        <td><button style={{borderRadius : '50%',color : '#fff',backgroundColor : '#1abc9c',fontWeight : 'bold',fontSize : '1.5rem',padding : 'auto'}} onClick={event=> increaseAllocation(props.name)}>+</button></td>
+        <td><button style={{borderRadius : '50%',color : '#fff',backgroundColor : 'red',fontWeight : 'bold',fontSize : '1.5rem',padding : 'auto'}} onClick={event=> decreaseAllocation(props.name)}>-</button></td>
         <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
         </tr>
     );
